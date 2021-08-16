7259dd578cb3bed187ace0f6c1302e7b1e30898c
Rongjian Zhang <pd4d10@gmail.com>
2021-08-16 12:24:24 +0800

    build: use webdev_proxy for browser router

--- unpub_web/dev.sh
+++ unpub_web/dev.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-dart run webdev serve
+dart pub global activate webdev 2.7.4
+dart pub global activate webdev_proxy 0.1.1
+dart pub global run webdev_proxy serve -- --auto=refresh --log-requests
--- unpub_web/web/main.dart
+++ unpub_web/web/main.dart
@@ -1,10 +1,9 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:unpub_web/app_component.template.dart' as ng;
-import 'package:unpub_web/constants.dart';
 import 'main.template.dart' as self;
 
-@GenerateInjector(isProduction ? routerProviders : routerProvidersHash)
+@GenerateInjector(routerProviders)
 final InjectorFactory injector = self.injector$Injector;
 
 void main() {
