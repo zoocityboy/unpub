97a2ba56026e93fb0e715e79163a08b032555af9
Rongjian Zhang <pd4d10@gmail.com>
2021-08-19 22:36:37 +0800

    refactor: file store

--- unpub/lib/src/file_store.dart
+++ unpub/lib/src/file_store.dart
@@ -1,31 +1,28 @@
 import 'dart:io';
-
 import 'package:path/path.dart' as path;
-
 import 'package_store.dart';
 
 class FileStore extends PackageStore {
   String baseDir;
   String Function(String name, String version)? getFilePath;
 
-  FileStore(this.baseDir,
-      {String Function(String name, String version)? this.getFilePath});
+  FileStore(this.baseDir, {this.getFilePath});
 
-  File _getTarballFile(String package, String version) {
-    return getFilePath == null
-        ? File(path.join(baseDir, '$package-$version.tar.gz'))
-        : File(path.join(baseDir, getFilePath!(package, version)));
+  File _getTarballFile(String name, String version) {
+    final filePath =
+        getFilePath?.call(name, version) ?? '$name-$version.tar.gz';
+    return File(path.join(baseDir, filePath));
   }
 
   @override
-  Future<void> upload(String package, String version, List<int> content) async {
-    var file = _getTarballFile(package, version);
+  Future<void> upload(String name, String version, List<int> content) async {
+    var file = _getTarballFile(name, version);
     await file.create(recursive: true);
     await file.writeAsBytes(content);
   }
 
   @override
-  Stream<List<int>> download(String package, String version) {
-    return _getTarballFile(package, version).openRead();
+  Stream<List<int>> download(String name, String version) {
+    return _getTarballFile(name, version).openRead();
   }
 }
